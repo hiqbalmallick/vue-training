197117986b4e83edf63b9368c9d51f63144f90aa
Hamza Iqbal Mallick <hiqbalmallick@gmail.com>
2021-09-21 17:27:03 +0500

    cart icon added to header

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,7 +2,14 @@
   <v-app>
     <side-nav />
 
-    <v-app-bar app />
+    <v-app-bar app>
+      <v-spacer />
+      <v-btn icon>
+        <v-badge overlap :content="cart.length ? cart.length : '0'">
+          <v-icon>mdi-cart</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
 
     <v-main>
       <v-container fluid>
@@ -15,11 +22,13 @@
 </template>
 
 <script lang="ts">
-  import Vue from "vue";
-  import SideNav from "./SideNav.vue";
+import Vue from "vue";
+import { mapState } from "vuex";
+import SideNav from "./SideNav.vue";
 
-  export default Vue.extend({
-    components: { SideNav },
-    name: "Layout",
-  });
+export default Vue.extend({
+  components: { SideNav },
+  computed: { ...mapState(["cart"]) },
+  name: "Layout",
+});
 </script>
