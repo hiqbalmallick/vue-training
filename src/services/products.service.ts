ce4fda46e3af65ec7172765c6c89e2178fe2a577
Hamza Iqbal Mallick <hiqbalmallick@gmail.com>
2021-09-21 17:23:35 +0500

    applied interface to service layer

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,11 +1,14 @@
+import { ProductItem } from "@/interfaces";
 import { get, post, put } from "@/plugins/axios";
 import { AxiosResponse } from "axios";
 
 const productService = {
-  get: (): Promise<AxiosResponse<any>> => get(`products`),
-  getById: (id: string): Promise<AxiosResponse<any>> => get(`products/${id}`),
-  add: (body: any): Promise<AxiosResponse<any>> => post(`products`, body),
-  update: (id: string, body: any): Promise<AxiosResponse<any>> =>
+  get: (): Promise<AxiosResponse<Array<ProductItem>>> => get(`products`),
+  getById: (id: string): Promise<AxiosResponse<ProductItem>> =>
+    get(`products/${id}`),
+  add: (body: ProductItem): Promise<AxiosResponse<void>> =>
+    post(`products`, body),
+  update: (id: string, body: ProductItem): Promise<AxiosResponse<void>> =>
     put(`products/${id}`, body),
 };
 export default productService;
