20c3bbac8b061ec26a1f5344eac76a37ccaeedc3
Hamza Iqbal Mallick <hiqbalmallick@gmail.com>
2021-09-21 17:54:58 +0500

    added skeleton on list view

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,105 +1,109 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="products"
-    sort-by="calories"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Products</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              New Item
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
+  <v-skeleton-loader :loading="loading" type="table">
+    <v-data-table
+      :headers="headers"
+      :items="products"
+      sort-by="calories"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Products</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                New Item
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
 
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.title"
-                      label="Title"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.category"
-                      label="Category"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.price"
-                      label="Price"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.rating.rate"
-                      label="Rating"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.title"
+                        label="Title"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.category"
+                        label="Category"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.price"
+                        label="Price"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.rating.rate"
+                        label="Rating"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5"
-              >Are you sure you want to delete this item?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel</v-btn
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5"
+                >Are you sure you want to delete this item?</v-card-title
               >
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >OK</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="isInCart(item) ? deleteFromCart(item) : addToCart(item)"
-        :color="isInCart(item) ? '#1976d2' : ''"
-      >
-        mdi-cart
-      </v-icon>
-      <!-- <v-icon small class="mr-2" @click="editItem(item)"> mdi-eye </v-icon> -->
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-    </template>
-    <template v-slot:item.image="{ item }">
-      <v-avatar size="36px">
-        <img :src="item.image" alt="Product Image" />
-      </v-avatar>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="fetchProducts"> Reset </v-btn>
-    </template>
-  </v-data-table>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Cancel</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                  >OK</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="isInCart(item) ? deleteFromCart(item) : addToCart(item)"
+          :color="isInCart(item) ? '#1976d2' : ''"
+        >
+          mdi-cart
+        </v-icon>
+        <!-- <v-icon small class="mr-2" @click="editItem(item)"> mdi-eye </v-icon> -->
+        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
+      <template v-slot:item.image="{ item }">
+        <v-avatar size="36px">
+          <img :src="item.image" alt="Product Image" />
+        </v-avatar>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="fetchList"> Reset </v-btn>
+      </template>
+    </v-data-table>
+  </v-skeleton-loader>
 </template>
 
 <script>
@@ -121,6 +125,7 @@ export default {
       { text: "Rating", value: "rating.rate" },
       { text: "Actions", value: "actions", sortable: false },
     ],
+    loading: true,
     editedIndex: -1,
     editedItem: {
       title: "",
@@ -157,7 +162,7 @@ export default {
   },
 
   created() {
-    this.fetchProducts();
+    this.fetchList();
   },
 
   methods: {
@@ -167,6 +172,11 @@ export default {
       "addToCart",
       "deleteFromCart",
     ]),
+    async fetchList() {
+      this.loading = true;
+      await this.fetchProducts();
+      this.loading = false;
+    },
     isInCart(item) {
       return findIndex(this.cart, item) !== -1;
     },
