1d5f1ac605806963928b084f38e8b6ef3de4905e
Hamza Iqbal Mallick <hiqbalmallick@gmail.com>
2021-09-21 17:29:30 +0500

    states moved from component to store

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -79,6 +79,15 @@
       </v-toolbar>
     </template>
     <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="isInCart(item) ? deleteFromCart(item) : addToCart(item)"
+        :color="isInCart(item) ? '#1976d2' : ''"
+      >
+        mdi-cart
+      </v-icon>
+      <!-- <v-icon small class="mr-2" @click="editItem(item)"> mdi-eye </v-icon> -->
       <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
       <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
     </template>
@@ -88,113 +97,125 @@
       </v-avatar>
     </template>
     <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset </v-btn>
+      <v-btn color="primary" @click="fetchProducts"> Reset </v-btn>
     </template>
   </v-data-table>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      dialog: false,
-      dialogDelete: false,
-      headers: [
-        { text: "Image", value: "image" },
-        {
-          text: "Title",
-          align: "start",
-          value: "title",
-        },
-        { text: "Price", value: "price" },
-        { text: "Category", value: "category" },
-        { text: "Rating", value: "rating.rate" },
-        { text: "Actions", value: "actions", sortable: false },
-      ],
-      products: [],
-      editedIndex: -1,
-      editedItem: {
-        title: "",
-        price: 0,
-        category: "",
-        rating: {
-          rate: 0,
-        },
+import { findIndex } from "@/utils/utils";
+import { mapActions, mapState } from "vuex";
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      { text: "Image", value: "image" },
+      {
+        text: "Title",
+        align: "start",
+        value: "title",
       },
-      defaultItem: {
-        title: "",
-        price: 0,
-        category: "",
-        rating: {
-          rate: 0,
-        },
-      },
-    }),
-
-    computed: {
-      formTitle() {
-        return this.editedIndex === -1 ? "New Item" : "Edit Item";
+      { text: "Price", value: "price" },
+      { text: "Category", value: "category" },
+      { text: "Rating", value: "rating.rate" },
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    editedIndex: -1,
+    editedItem: {
+      title: "",
+      price: 0,
+      category: "",
+      rating: {
+        rate: 0,
       },
     },
-
-    watch: {
-      dialog(val) {
-        val || this.close();
-      },
-      dialogDelete(val) {
-        val || this.closeDelete();
+    defaultItem: {
+      title: "",
+      price: 0,
+      category: "",
+      rating: {
+        rate: 0,
       },
     },
+  }),
 
-    created() {
-      this.initialize();
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+    ...mapState(["products", "cart"]),
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.fetchProducts();
+  },
+
+  methods: {
+    ...mapActions([
+      "fetchProducts",
+      "addProduct",
+      "addToCart",
+      "deleteFromCart",
+    ]),
+    isInCart(item) {
+      return findIndex(this.cart, item) !== -1;
     },
 
-    methods: {
-      async initialize() {
-        this.products = await this.$axios.get("products");
-      },
+    editItem(item) {
+      this.editedIndex = this.products.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
 
-      editItem(item) {
-        this.editedIndex = this.products.indexOf(item);
-        this.editedItem = Object.assign({}, item);
-        this.dialog = true;
-      },
+    deleteItem(item) {
+      this.editedIndex = this.products.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
 
-      deleteItem(item) {
-        this.editedIndex = this.products.indexOf(item);
-        this.editedItem = Object.assign({}, item);
-        this.dialogDelete = true;
-      },
+    deleteItemConfirm() {
+      this.products.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
 
-      deleteItemConfirm() {
-        this.products.splice(this.editedIndex, 1);
-        this.closeDelete();
-      },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
 
-      close() {
-        this.dialog = false;
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem);
-          this.editedIndex = -1;
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.products[this.editedIndex], this.editedItem);
+      } else {
+        this.addProduct({
+          ...this.editedItem,
+          image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
         });
-      },
-
-      closeDelete() {
-        this.dialogDelete = false;
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem);
-          this.editedIndex = -1;
-        });
-      },
-
-      save() {
-        if (this.editedIndex > -1) {
-          Object.assign(this.products[this.editedIndex], this.editedItem);
-        } else {
-          this.products.push(this.editedItem);
-        }
-        this.close();
-      },
+        // this.products.push(this.editedItem);
+      }
+      this.close();
     },
-  };
+  },
+};
 </script>
